e1ccc6204984deecb023f6e89d7b915b5ad2be3e
Arlynia <jdoherty1985@gmail.com>
2023-09-11 13:32:05 -0500

    Generic character and TODO

--- Dynamic-AI-Movement.py
+++ Dynamic-AI-Movement.py
@@ -7,9 +7,35 @@
 # Revisions Log:
 
 # --------------- Main Entry Begins Below -------------
+from math import pi
 import os
 from pathlib import Path
 
 # debug statement to verify file works
 print ("Hello World!")
 print("Nice to meet you!")
+
+class Character0:
+    sim_time=0
+    char_id=0
+    pos_x=0
+    pos_y=0
+    velocity_x=0
+    velocity_y=0
+    linear_x=0
+    linear_y=0
+    orientation=0
+    steering_behavior_code=0 #continue
+    collision_status=False
+    
+# steer, position, velocity, orientation, max.velocity, max.linear, target
+
+
+
+# TODO: SteeringOuput function
+        # Character classes
+        # Movement update function
+        # Seek class
+        # Flee class
+        # Arrive class
+        # Continue class
