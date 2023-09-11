cf4e656e06aac7a7ddbfab8409108773c4fe13e4
Arlynia <jdoherty1985@gmail.com>
2023-09-11 12:01:24 -0500

    Initial test

---
+++ Dynamic-AI-Movement.py
@@ -0,0 +1,14 @@
+# Names:
+# Class: CS 330 -01
+# Teacher: Jay Sebastian
+# 9/9/2023
+# Description: Program 1
+
+# Revisions Log:
+
+# --------------- Main Entry Begins Below -------------
+import os
+from pathlib import Path
+
+# debug statement to verify file works
+print ("Hello World!")
