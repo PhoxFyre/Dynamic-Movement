80477e4db3076502066c631b87b80961dc216cbe
PhoxFyre <kalebsanders007@gmail.com>
2023-09-18 13:57:32 -0500

    Removed excess spaces within functinos, reworked character1 and character4, corrected timeToTarget definition

--- Dynamic-AI-Movement.py
+++ Dynamic-AI-Movement.py
@@ -57,77 +57,70 @@ character4.steering_behavior_code=8 # Arrive
 
 # Set characters variables
 # Character 1 init
-character1.postion_x=0
-character1.position_z=0
-character1.velocity_x=0
-character1.velocity_z=0
-character1.linear_x=0
-character1.linear_z=0
-character1.orientation=0
-character1.collision_status=False
+character1.char_id = 2601
+character1.position = np.array([0,0])
+character1.velocity = np.array([0,0])
 
 # Character 2 init
-character2.position=np.array([-30,-50])
-character2.velocity=np.array([2,7])
-character2.orientation=pi/4
-character2.max_velocity=8
-character2.max_acceleration=1.5
-character2.target=1
+character2.char_id = 2602
+character2.position = np.array([-30,-50])
+character2.velocity = np.array([2,7])
+character2.orientation = pi / 4
+character2.max_velocity = 8
+character2.max_acceleration = 1.5
+character2.target = 1
 
 # Character 3 init
-character3.position=np.array([-50,40])
-character3.velocity=np.array([0,8])
-character3.orientation=3*pi/2
-character3.max_velocity=8
-character3.max_acceleration=2
-character3.target=1
+character3.char_id = 2603
+character3.position = np.array([-50,40])
+character3.velocity = np.array([0,8])
+character3.orientation = 3 * pi / 2
+character3.max_velocity = 8
+character3.max_acceleration = 2
+character3.target = 1
 
 # Character 4 init
-character4.position=np.array([-50,40])
-character4.velocity=np.array([0,8])
-character4.orientation=3*pi/2
-character4.max_velocity=8
-character4.max_acceleration=2
-character4.target=1
+character4.char_id = 2604
+character4.position = np.array([50,75])
+character4.velocity = np.array([-9,4])
+character4.orientation = pi
+character4.max_velocity = 10
+character4.max_acceleration = 2
+character4.target = 1
+character4.arrival_radius = 4
+character4.slowing_radius = 32
+character4.timeToTarget = 1
 
 # Steering Output
 class steeringOutput:
     linear: vector   # Linear acceleration, 2D vector # linear is variable, vector is type
     angular: float   # Angular acceleration, scalar
-
 # Movement Update
 def update(steering: steeringOutput, maxSpeed: float, time: float):
     # Update the position and orientation
     position += velocity * time
     orientation += rotation * time
- 
     # Update the velocity and rotation
     velocity += steering.linear * time
     rotation += steering.angular * time
- 
     # Check for speed above max and clip
     if velocity.length() > maxSpeed:
         velocity.normalize()
         velocity *= maxSpeed
 
-    
 # Dynamic Flee
 class flee:
     character: character2   # Position and orientation for character
     target: character1      # Position and orientation for target
     maxAcceleration: float  # Maximum acceleration rate for character
-    
     def getsteering() -> steeringOutput():
         # Create output structure
         result = steeringOutput()
-    
         # Get the direction to the target
         result.linear = character.position = target.position
-    
         # Accelerate at maximum rate
         result.linear.normalize()
         result.linear *= character.maxAcceleration
-    
         # Output steering
         result.angular = 0
         return result
@@ -137,69 +130,52 @@ class seek:
     character: character3           # Position and orientation for character
     target: character1              # Position and orientation for target
     maxAcceleration: float          # Maximum acceleration rate for character
-    
-    
     def getSteering() -> steeringOutput():
         # Create output strcuture
-        result = steeringOutput()
- 
+        result = steeringOutput() 
         # Get the direction to the target
         result.linear = target.position - character.position 
- 
         # Accelerate at maximum rate
         result.linear.normalize()
-        result.linear *= character.maxAcceleration
-        
+        result.linear *= character.maxAcceleration        
         # Output steering
         result.angular = 0
-        return result
-    
+        return result    
 
 # Dynamic Arrive
 class arrive:
     character: character4   # Position and orientation for character
     target: character1      # Position and orientation for target
-    
     maxAcceleration: float  
     maxSpeed: float         
-    
     targetRadius: float         # Arrival radius
     slowRadius: float           # Slowing-down radius
     timeToTarget: float = 0.1   # Time over which to achieve target speed
-    
     def getSteering() -> steeringOutput:
         result = steeringOutput()
-        
         # Get the direction and distance to the target
         direction = target.position - character.position
         distance = direction.length()
-        
         # Test for arrival
-        if distance < targetRadius:
+        if distance < character.targetRadius:
             return NULL
-        
         # Outside slowing-down (outer) radius, move at max speed
-        if distance > slowRadius:
-            targetSpeed = maxSpeed
-        
+        if distance > character.slowRadius:
+            targetSpeed = character.maxSpeed
         # Between radii, scale speed to slow down
         else:
-            targetSpeed = maxSpeed * distance / slowRadius
-            
+            targetSpeed = character.maxSpeed * distance / character.slowRadius
         # Target velocity combines speed and direction
         targetVelocity = direction
         targetVelocity.normalize()
         targetVelocity *= targetSpeed
-        
         # Accelerate to target velocity
         result.linear = targetVelocity - character.velocity
-        result.linear /= timeToTarget
-
+        result.linear /= character.timeToTarget
         # Test for too fast acceleration
         if result.linear.length() > character.maxAcceleration:
             result.linear.normalize()
             result.linear *= character.maxAcceleration
-        
         # Output steering
         result.angular = 0
         return result
